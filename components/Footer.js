86fdf075792c57ee6b915b03f9b1aba44f9e573d
hhafidzaahh <husnulhafidzah783@gmail.com>
2025-06-23 14:05:15 +0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ import styles from '@/styles/Footer.module.css'
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>&copy; {new Date().getFullYear()} Husnul Hapidah — Sistem Informasi UKM Mahasiswa</p>
+      <p>&copy; {new Date().getFullYear()} Husnul Hapidah — Sistem Informasi UKM</p>
     </footer>
   )
 }
